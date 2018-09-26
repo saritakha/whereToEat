1f99f4a19fddb58fa16c49c13c79da204de15f83
Jenna Kopra <jennakop@metropolia.fi>
2018-09-26 22:13:02 +0300

    Succesful api call with name and rating

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './Components/Home/Home';
 import Layout from './Layout/Layout';
 import { BrowserRouter , Route } from 'react-router-dom'
-
+import Restaurants from './Components/Api/Restaurants';
 class App extends Component {
   // constructor(props){
   //   super(props);
@@ -15,9 +15,10 @@ class App extends Component {
   render() {
     return (
      <BrowserRouter>
-      <div>
-       <Route exact= {true} path= '/' component= {Home}/>
-       <Route path= '/Layout' component= {Layout}/>
+       <div>
+         <Route exact= {true} path= '/' component= {Home}/>
+         <Route path= '/Layout' component= {Layout}/>
+         <Route path= '/Restaurants' component= {Restaurants}/>
       </div>
      </BrowserRouter>
     );
