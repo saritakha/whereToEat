965560db69b0d6c70bb275bed2278c9b7a38ecfb
vuquangtuan <vuquangtuan93@gmail.com>
2018-09-30 21:10:49 +0300

    google API showing direction

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import styles from './Search.css';
+import React from "react";
+import styles from "./Search.css";
 
 const Search = () => {
-    return(
-        <form className= {styles.form}>
-          <label>
-             Search for restaurant
-               <input type="text" name="name" />
-          </label>
-         <input type="submit" value="Submit" />
-        </form>
-    )
-}
+  navigator.geolocation.watchPosition(function(position) {
+    console.log(position);
+  });
+  return (
+    <div>
+      <form className={styles.form}>
+        <label>
+          Search for restaurant
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <button>
+        <a href="http://maps.google.com/maps?daddr=60.18009011393016,24.911799430847168">
+          Map
+        </a>
+      </button>
+    </div>
+  );
+};
 
 export default Search;
