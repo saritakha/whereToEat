6b280a49690e46c0670bdf6dbfa28be326eafd23
Jasmin Lehtinen <jasminle@metropolia.fi>
2018-10-01 11:57:16 +0300

    server side

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Home from './Components/Home/Home';
+import { BrowserRouter , Route } from 'react-router-dom'
+import Restaurants from './Components/Api/Restaurants';
+
+class App extends Component {
+  // constructor(props){
+  //   super(props);
+  // }
+  state = {
+    response: '',
+    food: []
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/hello");
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+
+      // Test stuff for server
+      /*<div className="App">
+        <p className="App-intro">{this.state.response}</p>
+      </div>*/
+
+     <BrowserRouter>
+       <div>
+         <Route exact= {true} path= '/' component= {Home}/>
+         <Route path= '/Restaurants' component= {Restaurants}/>
+      </div>
+     </BrowserRouter>
+    );
+  }
+}
+
+export default App;
