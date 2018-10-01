7cf6ff95b0ddb413f35ebdd8323effff68ef18fa
sarita khanal <saritakhanal533@gmail.com>
2018-10-01 15:39:19 +0300

    main file added

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Home from './Components/Home/Home';
 import { BrowserRouter , Route } from 'react-router-dom'
 import Restaurants from './Components/Api/Restaurants';
-// import Login from './Components/Login/Login';
+import Login from './Components/Login/Login';
+import Main from './Components/Main/Main';
 
 class App extends Component {
   // constructor(props){
@@ -40,7 +41,8 @@ class App extends Component {
        <div>
          <Route exact= {true} path= '/' component= { Home }/>
          <Route path= '/Restaurants' component= { Restaurants }/>
-         {/* <Route path= '/Login' component= { Login }/> */}
+         <Route path= '/Main' component= { Main }/>
+         <Route path= '/Login' component= { Login }/>
       </div>
      </BrowserRouter>
     );
