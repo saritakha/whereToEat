cc2dabb30068dd2cf9b444b5ff312a7f8b503194
sarita khanal <saritakhanal533@gmail.com>
2018-10-01 15:18:30 +0300

    server works

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './Components/Home/Home';
 import { BrowserRouter , Route } from 'react-router-dom'
 import Restaurants from './Components/Api/Restaurants';
-import Login from './Components/Login/Login';
+// import Login from './Components/Login/Login';
 import Main from './Components/Main/Main';
 
 class App extends Component {
@@ -41,7 +41,7 @@ class App extends Component {
        <div>
          <Route exact= {true} path= '/' component= { Home }/>
          <Route path= '/Restaurants' component= { Restaurants }/>
-         <Route path= '/Login' component= { Login }/>
+         {/* <Route path= '/Login' component= { Login }/> */}
          <Route path= '/Main' component= { Main }/>
       </div>
      </BrowserRouter>
--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,44 +1,10 @@
-// <<<<<<< HEAD
-// import React, {Component} from 'react';
-// import { NavLink } from 'react-router-dom';
-// import styles from './Search.css';
-// import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
-// class Search extends Component{
-//     state = {
-//         query: '',
-//         redirect: false
-//     }
 
-//     handleInputChange = () => {
-//         this.setState({
-//             query: this.search.value
-//         })
-//     }
 
-//     render(){ 
-//     return(
-       
-// <div>
-//   <FormGroup className= { styles.form} >
-//   <InputGroup>
-//     <FormControl
-//         placeholder= 'Input your location here.....'
-//         ref= {input => this.search = input}
-//         onChange= { this.handleInputChange }
-//     />
-//   <InputGroup.Addon>
-//    <NavLink  to= {'/Main'}><Glyphicon glyph="search"></Glyphicon></NavLink>
-//   </InputGroup.Addon>
-// </InputGroup>
-// </FormGroup>
-// </div>
-// )
-    
-//     }
-// }
-import React from "react";
-import styles from "./Search.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Search.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 const Search = () => {
   navigator.geolocation.watchPosition(function(position) {
@@ -46,19 +12,19 @@ const Search = () => {
   });
   return (
     <div>
-      <form className={styles.form}>
-        <label>
-          Search for restaurant
-          <input type="text" name="name" />
-
-        </label>
-        <input type="submit" value="Submit" />
-        <button>
-          <a href="http://localhost:3000/restaurants">
-            Where to eat?
-          </a>
-        </button>
-      </form>
+     <FormGroup className= { styles.form} >
+  <InputGroup>
+    <FormControl
+        placeholder= 'Input your location here.....'
+        ref= {input => this.search = input}
+        onChange= { this.handleInputChange }
+    />
+  <InputGroup.Addon>
+   <NavLink  to= {'/Main'}><Glyphicon glyph="search"></Glyphicon></NavLink>
+  </InputGroup.Addon>
+</InputGroup>
+</FormGroup>
+         
 
       <button>
         <a href="https://www.google.com/maps/dir/?api=1&origin=60.1689709,24.9556654&destination=60.206647,24.828305">
