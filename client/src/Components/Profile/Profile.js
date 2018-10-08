44bc923d39252af8ce01b5d16d334707e38c30c4
vuquangtuan <vuquangtuan93@gmail.com>
2018-10-08 21:26:49 +0300

    nav bar of profile now working

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -16,28 +16,24 @@ class Profile extends Component {
 
   render() {
     return (
-      <Root>
+      <div>
         <Navbar />
-        <div className={style.App}>
-          <div className={style.Profile}>
-            <h1 className={style.Name}>{this.state.username}</h1>
-            Email:
-            <button onClick={this.changeEmail}>Change email</button>
-            <input
-              placeholder={this.state.email}
-              onChange={value => this.setState({ email: value.target.value })}
-            />
-            Password:
-            <button onClick={this.changePassword}>Change password</button>
-            <input
-              type="password"
-              onChange={value =>
-                this.setState({ password: value.target.value })
-              }
-            />
-          </div>
+        <div className={style.Profile}>
+          <h1 className={style.Name}>{this.state.username}</h1>
+          Email:
+          <button onClick={this.changeEmail}>Change email</button>
+          <input
+            placeholder={this.state.email}
+            onChange={value => this.setState({ email: value.target.value })}
+          />
+          Password:
+          <button onClick={this.changePassword}>Change password</button>
+          <input
+            type="password"
+            onChange={value => this.setState({ password: value.target.value })}
+          />
         </div>
-      </Root>
+      </div>
     );
   }
 
