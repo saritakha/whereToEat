35a8383320fd7b4b356e31ee23b059d5f7a8501d
vuquangtuan <vuquangtuan93@gmail.com>
2018-10-08 21:20:12 +0300

    open a new tab when getting directions

--- client/src/Components/Api/Restaurants.js
+++ client/src/Components/Api/Restaurants.js
@@ -11,7 +11,7 @@ export default class Restaurants extends Component {
       error: null,
       index: 0,
       location: {
-        lat: 60.2208030,
+        lat: 60.220803,
         long: 24.805207
       }
     };
@@ -19,81 +19,95 @@ export default class Restaurants extends Component {
 
   componentDidMount() {
     let self = this;
-    navigator.geolocation.watchPosition(function(position) {
-      self.setState({
-        enableHighAccuracy: true,
-        location: {
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-          error:null
+    navigator.geolocation.watchPosition(
+      function(position) {
+        self.setState({
+          enableHighAccuracy: true,
+          location: {
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+            error: null
+          }
+        });
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, maximumAge: 1000, distanceFilter: 10 }
+    );
+
+    this.setState({
+      isLoading: true
+    });
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+        self.state.location.lat +
+        "," +
+        self.state.location.long +
+        "&radius=500&type=restaurant&key=AIzaSyDGgNbzA8m2lzd9ijxaGPhmoe-oVTr7nDk"
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Sorry, something went wrong");
         }
-      });
-    },
-    (error) => this.setState({ error: error.message }),
-    { enableHighAccuracy: true, maximumAge: 1000, distanceFilter: 10 }
-  );
+      })
+      .then(data =>
+        this.setState({
+          restaurants: data.results,
+          isLoading: false
+        })
+      )
+      .catch(error =>
+        this.setState({
+          error: null,
+          isLoading: false
+        })
+      );
+  }
 
-  this.setState({
-    isLoading: true
-  });
-  fetch(
-    "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +self.state.location.lat +"," +self.state.location.long + "&radius=500&type=restaurant&key=AIzaSyDGgNbzA8m2lzd9ijxaGPhmoe-oVTr7nDk"
-  )
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Sorry, something went wrong");
+  handleClick = (name, vic) => {
+    console.log(name);
+    window.open("https://www.google.com/maps/dir//" + name + ", +" + vic);
+  };
+  AddToFav = name => {
+    alert("Added to favourites: " + name);
+  };
+  CurrentLocDir = (lat, long, name, vic) => {
+    console.log("lat: " + lat + " long: " + long);
+    window.open(
+      "https://www.google.com/maps/dir/?api=1&origin=" +
+        lat +
+        "," +
+        long +
+        "&" +
+        "destination=" +
+        name +
+        ", +" +
+        vic
+    );
+  };
+
+  render() {
+    const { restaurants, isLoading, error } = this.state;
+    const location = this.state.location;
+    const restaurant = restaurants[this.state.index];
+    console.log(restaurants);
+    if (isLoading) {
+      return <p>Loading</p>;
     }
-  })
-  .then(data =>
-    this.setState({
-      restaurants: data.results,
-      isLoading: false
-    })
-  )
-  .catch(error =>
-    this.setState({
-      error: null,
-      isLoading: false
-    })
-  );
-}
+    if (error) {
+      return <p>{error.message}</p>;
+    }
 
-handleClick = (name, vic) => {
-  console.log(name);
-  window.location.href =
-  "https://www.google.com/maps/dir//" + name + ", +" + vic;
-};
-AddToFav = (name) => {
-  alert("Added to favourites: " + name);
-};
-CurrentLocDir = (lat,long,name,vic) => {
-  console.log("lat: " + lat + " long: " + long);
-  window.location.href ="https://www.google.com/maps/dir/?api=1&origin=" + lat + "," +long + "&" + "destination=" + name + ", +" + vic
-};
-
-render() {
-  const { restaurants, isLoading, error } = this.state;
-  const location = this.state.location;
-  const restaurant = restaurants[this.state.index];
-  console.log(restaurants);
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-
-  if (restaurants.length === 0 )return <div>loading</div>;
-  if(this.state.index === 15) this.setState({ index: this.state.index = 0});
-  return (
-
-    <div>
-      <Navbar />
-      <button
-        className= {styles.button}
-        onClick={() => this.setState({ index: this.state.index + 1 })}
+    if (restaurants.length === 0) return <div>loading</div>;
+    if (this.state.index === 15)
+      this.setState({ index: (this.state.index = 0) });
+    return (
+      <div>
+        <Navbar />
+        <button
+          className={styles.button}
+          onClick={() => this.setState({ index: this.state.index + 1 })}
         >
           No, show me another place
         </button>
@@ -101,39 +115,44 @@ render() {
           className={styles.button}
           onClick={() => {
             console.log("lat" + location.lat + "ja long " + location.long);
-            this.CurrentLocDir(location.lat, location.long, restaurant.name, restaurant.vicinity);
+            this.CurrentLocDir(
+              location.lat,
+              location.long,
+              restaurant.name,
+              restaurant.vicinity
+            );
           }}
-          >
-            Get directions from my current location
-          </button>
-          <button
-            className={styles.button}
-            onClick={() => {
-              this.handleClick(restaurant.name, restaurant.vicinity);
-            }}
-            >
-              Get me directions from anywhere!
-            </button>
-            <button
-              className={styles.button}
-              onClick={() => {
-                this.AddToFav(restaurant.name);
-              }}
-              >
-                Add to favourites
-              </button>
+        >
+          Get directions from my current location
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => {
+            this.handleClick(restaurant.name, restaurant.vicinity);
+          }}
+        >
+          Get me directions from anywhere!
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => {
+            this.AddToFav(restaurant.name);
+          }}
+        >
+          Add to favourites
+        </button>
 
-              <div className={styles.circle}>
-                <p> {restaurant.name}</p>
-                <p>{restaurant.vicinity}</p>
-                <p>
-                  Rating:
-                  {restaurant.rating}
-                  /5
-                </p>
-                <img src={restaurant.icon} alt="Restaurant picture" />
-              </div>
-            </div>
-          );
-        }
-      }
+        <div className={styles.circle}>
+          <p> {restaurant.name}</p>
+          <p>{restaurant.vicinity}</p>
+          <p>
+            Rating:
+            {restaurant.rating}
+            /5
+          </p>
+          <img src={restaurant.icon} alt="Restaurant picture" />
+        </div>
+      </div>
+    );
+  }
+}
